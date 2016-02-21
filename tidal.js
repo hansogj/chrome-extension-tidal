8045930193f797fd7df290d5d9db38b0908e6763
Hans Ole Gjerdrum <hansogj@gmail.com>
2016-02-21 22:55:57 +0100

    mark text in search field

--- tidal.js
+++ tidal.js
@@ -12,23 +12,30 @@
     }
   }
 
-  addEvent(document, "keydown", function (e) {
-    e = e || window.event;
-    if(e.code === "KeyS" && e.ctrlKey && e.shiftKey) {
-      e.preventDefault();
-      document.querySelector("[data-subview=search] input").focus();
-    } else if(e.code === "KeyA" && e.ctrlKey && e.shiftKey) {
-      e.preventDefault();
-      document.querySelector("[data-subview=albums] [data-subview=items] div a").focus()
-    } else {
-      return true;
+
+  (function ensuraeSarchFieldi() {
+    var searchField= document.querySelector("[data-subview=search] input");
+
+    function focusSearchField (e) {
+      e = e || window.event;
+      if(e.code === "KeyS" && e.ctrlKey && e.shiftKey) {
+        e.preventDefault();
+        //document.querySelector("[data-subview=search] input").focus();
+        searchField.focus();
+      } else if(e.code === "KeyA" && e.ctrlKey && e.shiftKey) {
+        e.preventDefault();
+        document.querySelector("[data-subview=albums] [data-subview=items] div a").focus()
+      } else {
+        return true;
+      }
     }
-  });
 
+    if (searchField) {
+      addEvent(searchField, "focus", function (e) {e.target.select();});
+      addEvent(document, "keydown", focusSearchField);
+    } else {
+      setTimeout(ensuraeSarchFieldi, 15);
+    }
+  }());
 
-
-
-  w.setTimeout(function () {
-    w.location.reload();
-  }, 1000*60*15);
 }(window));
